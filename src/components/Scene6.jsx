ad46d210845157fba8923edb92dad715a8061931
martin <martin@kali.kali>
2021-02-02 01:18:03 -0300

    adding glitches

--- src/components/Scene6.jsx
+++ src/components/Scene6.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 import Terminal from "terminal-in-react";
+import MainImage from "../images/me/glitch-02.gif";
+// import SpeechBubble from "../images/speechBubbles/glitch-bubble.gif";
+import "../css/scene6.css";
 
 export const Scene6 = () => {
   return (
     <div
       style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+        width: "100vw",
         height: "100vh",
+        backgroundColor: "white",
       }}
     >
       <Terminal
         color="green"
         backgroundColor="black"
         barColor="black"
-        style={{ fontWeight: "bold", fontSize: "1em" }}
+        style={{
+          fontWeight: "bold",
+          fontSize: "1em",
+          position: "fixed",
+          width: "100%",
+          height: "60%",
+          top: "10%",
+        }}
         commands={{
           "fix-this-page": () => "/scene7",
           // showmsg: "hola",
@@ -29,6 +38,16 @@ export const Scene6 = () => {
         }}
         msg="You can write anything here. Type help."
       />
+      <img
+        src={MainImage}
+        style={{ position: "absolute", bottom: 0, right: "-100px" }}
+        alt="me glitched"
+      />
+      {/* <img
+        className="speech-bubble-scene6"
+        src={SpeechBubble}
+        alt="speech bubble"
+      /> */}
     </div>
   );
 };
