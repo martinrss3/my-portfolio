b6aee04755f989711d721d0a70deb4fe6508f2ed
Martin Russo <martinrusso1@gmail.com>
2021-03-09 16:40:21 -0300

    media queries scene7 test ok

--- src/components/Scene7.jsx
+++ src/components/Scene7.jsx
@@ -2,19 +2,18 @@ import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { TweenMax } from "gsap";
 import Typewriter from "typewriter-effect";
-import Delay from "react-delay";
 import "../css/scene7.css";
 
 export const Scene7 = () => {
-  let nextScene = useRef(null);
+  let nextSceneRef = useRef(null);
 
-  const firstSpeech = "I was so close..!!";
+  const firstSpeech = `<span class="first-speech-scene7">I was so close..!!</span>`;
 
   const [buttons, setButtons] = useState(false);
 
   useEffect(() => {
     TweenMax.fromTo(
-      nextScene,
+      nextSceneRef,
       1,
       {
         delay: 2,
@@ -28,37 +27,33 @@ export const Scene7 = () => {
   }, []);
 
   return (
-    <div className="container-scene7">
-      <Delay wait={3000}>
-        <div>
-          <img className="main-img-scene7" alt="bad guy" />
-        </div>
-      </Delay>
-      <Delay wait={5500}>
-        <div>
-          <img className="speech-bubble-scene7" alt="speech bubble" />
-          <div className="typed-scene7">
-            <Typewriter
-              options={{
-                delay: 75,
-              }}
-              onInit={(typewriter) => {
-                typewriter
-                  .typeString(firstSpeech)
-                  .callFunction(() => {
-                    setButtons(!buttons);
-                  })
-                  .start();
-              }}
-            />
-          </div>
-        </div>
-      </Delay>
+    <div className="container bg-scene7">
+      <div>
+        <img className="main-image img-scene7" alt="bad guy" />
+      </div>
+
+      <div className="text txt-scene7">
+        <Typewriter
+          options={{
+            delay: 75,
+            cursor: "",
+          }}
+          onInit={(typewriter) => {
+            typewriter
+              .typeString(firstSpeech)
+              .callFunction(() => {
+                setButtons(!buttons);
+              })
+              .start();
+          }}
+        />
+      </div>
+
       <div>
         <Link to="/scene8">
           <button
             ref={(el) => {
-              nextScene = el;
+              nextSceneRef = el;
             }}
             className={buttons ? "next-scene" : "next-scene-changed"}
           >
