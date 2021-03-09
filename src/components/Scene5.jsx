401286daa61861e217accce188b45d3ac2fd2a22
Martin Russo <martinrusso1@gmail.com>
2021-03-09 11:34:52 -0300

    media queries scene5 test ok

--- src/components/Scene5.jsx
+++ src/components/Scene5.jsx
@@ -1,27 +1,23 @@
 import React, { useEffect, useRef, useState } from "react";
-import { TweenMax, Power3, TimelineLite } from "gsap";
 import { Link } from "react-router-dom";
+import { TweenMax, Power3, TimelineLite } from "gsap";
 import Typewriter from "typewriter-effect";
 import Delay from "react-delay";
 import "../css/scene5.css";
 
 export const Scene5 = () => {
-  let mainImage = useRef(null);
-  let speechBubble1 = useRef(null);
-  let speechBubble2 = useRef(null);
-  let nextScene = useRef(null);
-  let backScene = useRef(null);
+  let imageRef = useRef(null);
+  let nextSceneRef = useRef(null);
+  let backSceneRef = useRef(null);
 
-  const firstSpeech = "this will be very easy...";
-  const secondSpeech =
-    "this guy \n is trying to \n break the code... \n We must stop him!";
+  const firstSpeech = `<span class="first-speech-scene5">this will be \n very easy...</span>`;
+  const secondSpeech = `<span class="second-speech-scene5">this guy \n is trying to \n break the code... \n We must stop him!</span>`;
 
-  const [speechBubble, setSpeechBubble] = useState(false);
   const [text, setText] = useState(false);
   const [buttons, setButtons] = useState(false);
 
   useEffect(() => {
-    TweenMax.staggerTo([mainImage, speechBubble2], 0.9, {
+    TweenMax.staggerTo(imageRef, 0.9, {
       opacity: 1,
       x: 0,
       ease: Power3.easeInOut,
@@ -29,20 +25,7 @@ export const Scene5 = () => {
     });
 
     TweenMax.fromTo(
-      speechBubble1,
-      1,
-      {
-        delay: 1,
-        opacity: 0,
-      },
-      {
-        delay: 1,
-        opacity: 1,
-      }
-    );
-
-    TweenMax.fromTo(
-      [nextScene, backScene],
+      [nextSceneRef, backSceneRef],
       1,
       {
         delay: 2,
@@ -55,90 +38,70 @@ export const Scene5 = () => {
     );
 
     let tl = new TimelineLite();
-    tl.to([mainImage, speechBubble2], 0, { x: -1000 });
+    tl.to(imageRef, 0, { x: -1000 });
   }, []);
 
   return (
-    <div className="container-scene5">
+    <div className="container bg-scene5">
       <div>
-        <img className="main-image-scene5" alt="bad guy" />
+        <img className="main-image img-scene5" alt="bad guy" />
       </div>
-      <div>
-        <img
-          className={
-            speechBubble
-              ? "speech-bubble-scene5-changed"
-              : "speech-bubble-scene5"
-          }
-          ref={(el) => {
-            speechBubble1 = el;
-          }}
-          alt="speech bubble"
-        />
-        <div>
-          <Delay wait={1400}>
-            <div className={text ? "text-scene5-changed" : "text-scene5"}>
-              <Typewriter
-                options={{
-                  delay: 70,
-                }}
-                onInit={(typewriter) => {
-                  typewriter
-                    .typeString(firstSpeech)
-                    .start()
-                    .pauseFor(1800)
-                    .callFunction(() => {
-                      setSpeechBubble(!speechBubble);
-                      setText(!text);
-                    });
-                }}
-              />
-            </div>
-          </Delay>
+
+      <Delay wait={1400}>
+        <div
+          className={text ? "toggle-text txt-scene5-chg" : "text txt-scene5"}
+        >
+          <Typewriter
+            options={{
+              delay: 70,
+              cursor: "",
+            }}
+            onInit={(typewriter) => {
+              typewriter
+                .typeString(firstSpeech)
+                .start()
+                .pauseFor(1700)
+                .callFunction(() => {
+                  setText(!text);
+                });
+            }}
+          />
         </div>
-      </div>
+      </Delay>
 
       <div
         className="frame"
         ref={(el) => {
-          mainImage = el;
+          imageRef = el;
         }}
       >
-        <img className="main-image2-scene5" alt="me" />
+        <img className="main-image img2-scene5" alt="me" />
       </div>
-      <div>
-        <img
-          className="speech-bubble2-scene5"
-          ref={(el) => {
-            speechBubble2 = el;
-          }}
-          alt="speech bubble"
-        />
-        <div>
-          <Delay wait={5700}>
-            <div className="text2-scene5">
-              <Typewriter
-                options={{
-                  delay: 50,
-                }}
-                onInit={(typewriter) => {
-                  typewriter
-                    .typeString(secondSpeech)
-                    .callFunction(() => {
-                      setButtons(!buttons);
-                    })
-                    .start();
-                }}
-              />
-            </div>
-          </Delay>
+
+      <Delay wait={6300}>
+        <div className="text txt2-scene5">
+          <Typewriter
+            options={{
+              delay: 50,
+              cursor: "",
+            }}
+            onInit={(typewriter) => {
+              typewriter
+                .typeString(secondSpeech)
+                .callFunction(() => {
+                  setButtons(!buttons);
+                })
+                .start();
+            }}
+          />
         </div>
-      </div>
+      </Delay>
+
       <div>
         <Link to="/scene4">
           <button
             ref={(el) => {
-              backScene = el;
+              backSceneRef = el;
             }}
             className={buttons ? "back-scene" : "back-scene-changed"}
           >
@@ -148,7 +111,7 @@ export const Scene5 = () => {
         <Link to="/scene6">
           <button
             ref={(el) => {
-              nextScene = el;
+              nextSceneRef = el;
             }}
             className={buttons ? "next-scene" : "next-scene-changed"}
           >
