f49119dd3d0e60b68bb43ddbd41d7b323886c19e
Martin Russo <martinrusso1@gmail.com>
2021-03-18 18:45:40 -0300

    update cv

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Contact } from "./Contact";
 import "../css/about.css";
-import bg from "../images/parallax/bg-about.jpg";
+import bg from "../images/backgrounds/bg-about.jpg";
 import moon from "../images/parallax/moon.png";
 import cloud1 from "../images/parallax/cloud1.png";
 import cloud2 from "../images/parallax/cloud2.png";
