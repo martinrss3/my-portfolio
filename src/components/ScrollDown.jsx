4d7df6f1f20cb69ba2077eca37b1b6f38f3880ac
Martin Russo <martinrusso1@gmail.com>
2021-05-10 15:24:41 -0300

    add scroll down text

--- src/components/ScrollDown.jsx
+++ src/components/ScrollDown.jsx
@@ -7,11 +7,11 @@ export const ScrollDown = () => {
   gsap.registerPlugin(ScrollTrigger);
 
   useEffect(() => {
-    gsap.to(".scrolldown", {
+    gsap.to(".scrolldown-wrapper", {
       scrollTrigger: {
         scrub: true,
       },
-      opacity: 0,
+      opacity: -1,
     });
   }, []);
 
@@ -22,7 +22,9 @@ export const ScrollDown = () => {
           <circle className="scrolldown-p1" cx="5" cy="15" r="2" />
           <circle className="scrolldown-p2" cx="5" cy="15" r="2" />
         </svg>
+        
       </div>
+      <p className="scrolldown-text">Scroll down</p>
     </div>
   );
 };
